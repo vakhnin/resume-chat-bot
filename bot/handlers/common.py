8b6e1530e27fb6f79cbff441d5f70cb1ac58df70
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-07-14 08:52:34 +0300

    Code reorganization

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -1,11 +1,11 @@
-from aiogram import Dispatcher, html
+from aiogram import html, Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
-dp = Dispatcher()
+router = Router()
 
 
-@dp.message(CommandStart())
+@router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     """
     This handler receives messages with `/start` command
@@ -16,18 +16,3 @@ async def command_start_handler(message: Message) -> None:
     # method automatically or call API method directly via
     # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
-
-
-@dp.message()
-async def echo_handler(message: Message) -> None:
-    """
-    Handler will forward receive a message back to the sender
-
-    By default, message handler will handle all message types (like a text, photo, sticker etc.)
-    """
-    try:
-        # Send a copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
--- bot/main.py
+++ bot/main.py
@@ -3,11 +3,11 @@ import logging
 import os
 import sys
 
-from aiogram import Bot
+from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from bot.handlers.common import dp
+from bot.handlers import unknown, common
 
 TOKEN = os.environ.get('ENV_TOKEN')
 
@@ -19,6 +19,12 @@ if not TOKEN:
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher(bot=bot)
+
+    # Registration of handlers
+    dp.include_router(common.router)
+    # Registering a handler for an unrecognized command (must be last)
+    dp.include_router(unknown.router)
 
     # And the run events dispatching
     await dp.start_polling(bot)
