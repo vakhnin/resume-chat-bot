67d789a4143ef753d77ae53aeefe322ae8888ff0
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-07-16 08:33:47 +0300

    Image command

---
+++ bot/handlers/images.py
@@ -0,0 +1,11 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(Command('image'))
+async def command_image_handler(message: Message) -> None:
+    photo_id = 'AgACAgIAAxkDAAOdZpYEvOq-Cv2faST-toG4r4MBoewAAjvrMRvRJLFIqOU1KifnMMQBAAMCAANtAAM1BA'
+    await message.answer_photo(photo=photo_id, caption="Here is your image!")
