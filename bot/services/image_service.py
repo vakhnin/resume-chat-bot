68849a332173eb595595289ffe3fef8e190456a1
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-07-16 09:53:04 +0300

    Improve preload images and get ID

--- bot/services/image_service.py
+++ bot/services/image_service.py
@@ -1,14 +1,11 @@
-from aiogram import Bot
 from aiogram.types import FSInputFile
 
-from bot.config.settings import API_TOKEN
 
+async def upload_image(bot, file_path) -> str:
+    if not file_path.exists():
+        return None
+    photo: FSInputFile = FSInputFile(file_path)
+    message = await bot.send_photo(chat_id=111111111, photo=photo)
 
-
-async def upload_image(bot ,file_path: str) -> str:
-    with open(file_path, 'rb') as file:
-        photo = FSInputFile(file_path)
-        message = await bot.send_photo(chat_id=111111111, photo=photo)
-
-        photo_id = message.photo[-1].file_id
-        return photo_id
+    photo_id = message.photo[-1].file_id
+    return photo_id
--- bot/utils/preload_images.py
+++ bot/utils/preload_images.py
@@ -9,13 +9,19 @@ bot = Bot(token=API_TOKEN)
 
 
 async def main():
-    image_paths = [IMAGES_DIR / 'telegram-dark.png', IMAGES_DIR / 'telegram-light.png']
+    image_paths = [
+        IMAGES_DIR / 'test_images' / 'telegram-dark.png',
+        IMAGES_DIR / 'test_images' / 'telegram-light.png',
+    ]
     image_ids = []
 
     for image_path in image_paths:
         image_id = await upload_image(bot, image_path)
-        image_ids.append(image_id)
-        print(f"Uploaded {image_path}, got file_id: {image_id}")
+        if image_id:
+            image_ids.append(image_id)
+            print(f'Uploaded {image_path}, got file_id: {image_id}')
+        else:
+            print(f'{image_path} not exists')
 
     with open('image_ids.txt', 'w') as f:
         for image_id in image_ids:
