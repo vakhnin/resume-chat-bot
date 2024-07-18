8c9c3a8b9d2f4fd757e552ba67e3390271bdb45d
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-07-18 12:25:20 +0300

    Send images command

--- bot/handlers/images.py
+++ bot/handlers/images.py
@@ -1,11 +1,22 @@
+from typing import List
+
 from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, InputMediaPhoto
 
 router: Router = Router()
 
 
 @router.message(Command('image'))
 async def command_image_handler(message: Message) -> None:
-    photo_id = 'AgACAgIAAxkDAAOdZpYEvOq-Cv2faST-toG4r4MBoewAAjvrMRvRJLFIqOU1KifnMMQBAAMCAANtAAM1BA'
+    photo_id = 'AgACAgIAAxkDAAMWZpja1TxGD4UvdbRtZszWBCHY66sAAiXaMRssQ8hIPqJjpLWs9zABAAMCAANtAAM1BA'
     await message.answer_photo(photo=photo_id, caption="Here is your image!")
+
+
+@router.message(Command('images'))
+async def command_image_handler(message: Message) -> None:
+    media_group: List[InputMediaPhoto] = [
+        InputMediaPhoto(media='AgACAgIAAxkDAAMVZpja1JtgJE4EuYeBeo9rrS6DN0gAAiTaMRssQ8hIfpFqBFH3lrUBAAMCAANtAAM1BA'),
+        InputMediaPhoto(media='AgACAgIAAxkDAAMWZpja1TxGD4UvdbRtZszWBCHY66sAAiXaMRssQ8hIPqJjpLWs9zABAAMCAANtAAM1BA')
+    ]
+    await message.answer_media_group(media=media_group, caption="Here is your image!")
