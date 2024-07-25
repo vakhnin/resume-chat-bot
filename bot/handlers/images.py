6c97cfb8b1cb4eadbfadb59b275929d7e5bb1172
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-07-25 11:32:27 +0300

    Get param in /images command

--- bot/handlers/images.py
+++ bot/handlers/images.py
@@ -1,22 +1,31 @@
-from typing import List
+from typing import List, Optional, NoReturn, Type
 
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message, InputMediaPhoto
 
+from bot.config.settings import db_session
+from bot.db.models import Image
+
 router: Router = Router()
 
 
 @router.message(Command('image'))
-async def command_image_handler(message: Message) -> None:
-    photo_id = 'AgACAgIAAxkDAAMWZpja1TxGD4UvdbRtZszWBCHY66sAAiXaMRssQ8hIPqJjpLWs9zABAAMCAANtAAM1BA'
-    await message.answer_photo(photo=photo_id, caption="Here is your image!")
+async def command_image_handler(message: Message) -> NoReturn:
+    image: Optional[Image] = db_session.query(Image).first()
+    if image:
+        await message.answer_photo(photo=image.server_image_id, caption="Here is your image!")
 
 
 @router.message(Command('images'))
-async def command_image_handler(message: Message) -> None:
-    media_group: List[InputMediaPhoto] = [
-        InputMediaPhoto(media='AgACAgIAAxkDAAMVZpja1JtgJE4EuYeBeo9rrS6DN0gAAiTaMRssQ8hIfpFqBFH3lrUBAAMCAANtAAM1BA'),
-        InputMediaPhoto(media='AgACAgIAAxkDAAMWZpja1TxGD4UvdbRtZszWBCHY66sAAiXaMRssQ8hIPqJjpLWs9zABAAMCAANtAAM1BA')
-    ]
-    await message.answer_media_group(media=media_group, caption="Here is your image!")
+async def command_image_handler(message: Message) -> NoReturn:
+    command_parts: List[str] = message.text.split(maxsplit=1)
+    if len(command_parts) < 2:
+        return
+    catalog: str=command_parts[1]
+    images: List[Type[Image]] = db_session.query(Image).filter(Image.catalog == catalog).all()
+    if images:
+        media_group: List[InputMediaPhoto] = []
+        for image in images:
+            media_group.append(InputMediaPhoto(media=image.server_image_id, caption=image.file_name))
+        await message.answer_media_group(media=media_group, caption="Here is your images!")
