9528c593a5a909a5d876d721b5cc1d0ba8eef060
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-08-04 08:44:28 +0300

    Renaming

--- bot/db/models.py
+++ bot/db/models.py
@@ -14,7 +14,7 @@ class Base(DeclarativeBase):
 class Image(Base):
     __tablename__ = 'image'
     __table_args__ = (UniqueConstraint('catalog', 'file_name'),)
-    image_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     catalog: Mapped[Optional[str]]
     file_name: Mapped[str]
     caption: Mapped[Optional[str]]
@@ -22,7 +22,7 @@ class Image(Base):
     server_image_id_date: Mapped[str] = mapped_column(DateTime, default=datetime.now)
 
     def __repr__(self) -> str:
-        return f"Image(id={self.image_id!r}, catalog={self.catalog!r}, file_name={self.file_name!r})"
+        return f"Image(id={self.id!r}, catalog={self.catalog!r}, file_name={self.file_name!r})"
 
 
 class ProjectListAssociation(Base):
