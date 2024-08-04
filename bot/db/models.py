9a4b5b59eb9f8d3790c02308561b79e14726a40a
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-08-04 08:34:13 +0300

    Renaming

--- bot/db/models.py
+++ bot/db/models.py
@@ -25,32 +25,30 @@ class Image(Base):
         return f"Image(id={self.image_id!r}, catalog={self.catalog!r}, file_name={self.file_name!r})"
 
 
-class ProjectsListProjectstAssociation(Base):
-    __tablename__ = "projects_list_project_association_table"
-    project_id: Mapped[int] = mapped_column(ForeignKey("project.project_id"), primary_key=True)
-    projects_list_id: Mapped[int] = mapped_column(
-        ForeignKey("project_list.projects_list_id"), primary_key=True
-    )
+class ProjectListAssociation(Base):
+    __tablename__ = "projects_list_association"
+    project_id: Mapped[int] = mapped_column(ForeignKey("project.id"), primary_key=True)
+    projects_list_id: Mapped[int] = mapped_column(ForeignKey("project_list.id"), primary_key=True)
     order: Mapped[int]
     project: Mapped["Project"] = relationship(back_populates="projects_list")
     projects_list: Mapped["ProjectsList"] = relationship(back_populates="projects")
 
 
 class Project(Base):
-    __tablename__ = 'project'
-    project_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    __tablename__ = "project"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     button_title: Mapped[str]
     description_for_skills: Mapped[str]
     shot_description: Mapped[str]
     description: Mapped[str]
-    projects_list: Mapped[list['ProjectsList']] \
-        = relationship("ProjectsListProjectstAssociation", back_populates="project")
+    projects_list: Mapped[list["ProjectsList"]] \
+        = relationship("ProjectListAssociation", back_populates="project")
 
 
 class ProjectsList(Base):
-    __tablename__ = 'project_list'
-    projects_list_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    __tablename__ = "project_list"
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
     projects: Mapped[list['Project']] \
-        = relationship("ProjectsListProjectstAssociation", back_populates="projects_list",
-                       order_by="ProjectsListProjectstAssociation.order")
+        = relationship("ProjectListAssociation", back_populates="projects_list",
+                       order_by="ProjectListAssociation.order")
